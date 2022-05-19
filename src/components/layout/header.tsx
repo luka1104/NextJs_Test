f709e4f24920e6dfee97abf81e90425af9def544
luka1104 <skyward.luke11@gmail.com>
2022-05-19 15:53:31 -0700

    discord auth test

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,19 +4,8 @@ import { HStack } from '@chakra-ui/react';
 
 const Header = () => {
   return (
-    <HStack
-      as='header'
-      position='fixed'
-      top='0'
-      p={8}
-      zIndex='tooltip'
-      justify='space-between'
-      align='center'
-      w='100%'
-    >
-      <ThemeButton />
-      <LanguagesButton />
-    </HStack>
+    <>
+    </>
   );
 };
 
